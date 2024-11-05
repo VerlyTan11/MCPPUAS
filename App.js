92d99fff8a0d6df88fd1ba4de0e2bdaa4dd418c2
VerlyTan11 <55240962+VerlyTan11@users.noreply.github.com>
2024-11-05 10:52:17 +0700

    change template

---
+++ App.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import "./global.css"
+
+export default function App() {
+  return (
+    <View>
+      <Text className="text-yellow-400">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
